f11aa96a5b5f56e82baa89442d28bd68bec90f67
Angelo Vargas <angelo@nivler.com>
2015-06-08 10:03:59 -0500

    renamed bashrc-extras to bashrc.d

--- bashrc.sh
+++ bashrc.sh
@@ -6,7 +6,7 @@ _reload()
 }
 
 DIR="$(dirname $(readlink -f ${BASH_SOURCE[0]}))"
-EXTRAS="$DIR/bashrc-extras"
+EXTRAS="$DIR/bashrc.d"
 
 export PATH=$PATH:$HOME/bin
 
@@ -16,7 +16,6 @@ for f in $( find "$EXTRAS" -type f -print | sort )
 do
  . $f
 done
-
 
 # clean up $PATH, remove duplicates but keep order
 export PATH=$(echo $PATH | grep -oE ':?[^:]+' | sed 's/^://g' | nl|sort -k2 -k 1,1nr|uniq -f1|sort -n|cut -f2 | tr "\n" ":")
